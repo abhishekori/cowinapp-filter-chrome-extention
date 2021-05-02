dda8d3ed3bb648d86c83565a1b06d2688f6cfa0b
abhishekori <abhikori1994@gmail.com>
2021-05-02 07:20:32 +0530

    basic setup

---
+++ filter_vaccine_slots.js
@@ -0,0 +1,9 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+    alert('working?');
+    var rows = document.getElementsByClassName('row ng-star-inserted');
+    debugger;
+   rows.filter((row) => {
+
+
+   });
+});
